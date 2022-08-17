cac00d34f2a83fc3634843221a6b4fc8fe4d0eb4
bin <1072700702@qq.com>
2022-08-17 13:07:45 +0800

    框架父工程和用户环境搭建完成

---
+++ chou-parent/chou-UserManagerserver/src/test/java/com/chou/ChouUserManagerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChouUserManagerserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ chou-parent/chou-Util/src/test/java/com/chou/ChouUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChouUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
